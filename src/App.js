e70ad25ab53a6db90ad11773fb5a717f09db5bb2
Sai Chandan <saichandhan.yata44@gmail.com>
2024-02-14 16:14:09 +0530

    Output

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { setBooks } from './store/book';
+import { Container } from './components';
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect( () => {
+    let data = JSON.parse(sessionStorage.getItem('books'));
+    if ( !data ) {
+      ( async () => {
+        const res = await fetch('https://openlibrary.org/people/mekBot/books/already-read.json');
+        data = await res.json();
+        data = data['reading_log_entries'];
+  
+        data = data.map((book, index) => {
+          return {
+            'id': index,
+            'cover': `https://covers.openlibrary.org/b/id/${book.work.cover_id}-M.jpg`,
+            'title': book.work.title,
+            'author': book.work.author_names,
+            'year': book.work.first_publish_year,
+            'is_read': false,
+          }
+        });
+        sessionStorage.setItem('books', JSON.stringify(data));
+        
+      })();
+    }
+    
+
+    dispatch(setBooks(data));
+  },[]);
+
   const state = useSelector(state => state.books.value);
-  console.log(state);
+
   return (
     <div className="App">
+    <Container>
       Hello
+    </Container>
     </div>
   );
 }
--- src/store/book.js
+++ src/store/book.js
@@ -8,11 +8,27 @@ export const bookSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    
+    setBooks(state, action) {
+      state.value = action.payload;
+    },
+
+    searchBooks(state, action) {
+      state.value = state.value.filter(book => book.title?.includes(action.payload));
+    },
+
+    changeStatus(state, action) {
+      state.value = state.value.map(book => {
+        if (book.id === action.payload) {
+          book.is_read =!book.is_read;
+        }
+        return book;
+      });
+      sessionStorage.setItem('books', JSON.stringify(state.value));
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-// export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { setBooks, searchBooks, changeStatus } = bookSlice.actions
 
 export default bookSlice.reducer;
